99bb7f55c9940aa3a27767838f76cc5b09611576
Anatoly Thernytskyi <mendor1990@gmail.com>
2024-01-19 16:26:53 +0200

    fs-phone versions conflict fix

--- src/app/modules/manage/fs-2f-manage.module.ts
+++ src/app/modules/manage/fs-2f-manage.module.ts
@@ -1,34 +1,35 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
-import { FsListModule } from '@firestitch/list';
+import { FsCountryModule } from '@firestitch/country';
 import { FsDialogModule } from '@firestitch/dialog';
 import { FsFormModule } from '@firestitch/form';
-import { FsSkeletonModule } from '@firestitch/skeleton';
 import { FsLabelModule } from '@firestitch/label';
+import { FsListModule } from '@firestitch/list';
 import { FsPhoneModule } from '@firestitch/phone';
-import { FsCountryModule } from '@firestitch/country';
+import { FsSkeletonModule } from '@firestitch/skeleton';
 
 
 import { CodeInputModule } from '../code-input';
+import { DefautledModule } from '../defaulted/defaulted.module';
+import { ResendModule } from '../resend/resend.module';
+import { Fs2faVerificationMethodsModule } from '../verification-methods';
+import { Fs2faVerificationModule } from '../verification/fs-2fa-verification.module';
+
 import { Fs2faManageComponent } from './components/2fa-manage';
-import { ManageMethodsComponent } from './components/manage-methods';
-import { NumbersComponent } from './components/numbers';
 import { AppComponent } from './components/app';
 import { EmailComponent } from './components/email';
-import { NumberComponent } from './components/number';
 import { EmailsComponent } from './components/emails';
-import { ResendModule } from '../resend/resend.module';
-import { DefautledModule } from '../defaulted/defaulted.module';
-import { Fs2faVerificationModule } from '../verification/fs-2fa-verification.module';
-import { Fs2faVerificationMethodsModule } from '../verification-methods';
+import { ManageMethodsComponent } from './components/manage-methods';
+import { NumberComponent } from './components/number';
+import { NumbersComponent } from './components/numbers';
 import { QrCodeComponent } from './components/qr-code';
 
 
