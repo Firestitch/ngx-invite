53198e4f8ee5cf6552ca5f61b5b3d0eaedc904d2
Anatoly Thernytskyi <mendor1990@gmail.com>
2024-03-06 14:31:42 +0200

    code input updates

--- src/app/modules/code-input/code-input.module.ts
+++ src/app/modules/code-input/code-input.module.ts
@@ -3,13 +3,18 @@ import { FormsModule } from '@angular/forms';
 
 import { FsCodeInputModule } from '@firestitch/code-input';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { CodeInputComponent } from './components/code-input';
 
 
 @NgModule({
   imports: [
     FormsModule,
-    
+
+    MatFormFieldModule,
+    MatInputModule,
+
     FsCodeInputModule,
   ],
   declarations: [
