99521ba128afc1a2f2193489c310fdb70e3f5530
Raymond Gigliotti <ray@firestitch.com>
2025-01-07 15:42:29 -0500

    NIK-T26_Owner_Invite-_Dialogue_keeps_on_loading_(_Invite_already_used)

--- src/app/modules/invite/views/error/error.component.ts
+++ src/app/modules/invite/views/error/error.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
 } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -13,6 +14,8 @@ import { MatCardModule } from '@angular/material/card';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
+    RouterModule,
+    
     MatCardModule,
     MatButtonModule,
   ],
